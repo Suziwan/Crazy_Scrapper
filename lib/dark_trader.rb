9d02b6377c88f78ab69d633700089d2ee6842e32
Suziwan <christe.suzanne@gmail.com>
2023-01-26 19:18:23 +0000

    Dark_trader finished

--- lib/dark_trader.rb
+++ lib/dark_trader.rb
@@ -3,3 +3,30 @@ require 'open-uri'
 PAGE_URL = "https://coinmarketcap.com/all/views/all/"
 
 page = Nokogiri::HTML(URI.open(PAGE_URL))
+
+def scrap_names(page)
+  names_array = [] # Creating an empty array for names
+  names = page.xpath('//tbody//td[3]/div').each do |link| # Scraping the third column (cryptos' names)
+    names_array << link.content
+  end
+  names_array
+end
+
+def scrap_values(page)
+  values_array = [] # Creating an empty array for values
+  values = page.xpath('//tbody//td[5]/div').each do |link| # Scraping the fifth column (cryptos' values)
+    values_array << link.content
+  end
+  values_array
+end
+
+def link_crypto(names, values)
+  crypto_hash = names.zip(values).to_h
+  crypto_hash
+end
+
+def perform(page)
+  link_crypto(scrap_names(page), scrap_values(page))
+end
+
+perform(page)
--- spec/dark_trader_spec.rb
+++ spec/dark_trader_spec.rb
@@ -1,7 +1,40 @@
 require_relative '../lib/dark_trader'
 
-describe "the method to test" do
-  it "does the test" do
-    #expect(hello).to eq("Hello world!")
+PAGE_URL = "https://coinmarketcap.com/all/views/all/"
+page = Nokogiri::HTML(URI.open(PAGE_URL))
+
+describe "the scrap_names method" do
+  it "gives at least 3 names" do
+    expect(scrap_names(page).length).to be > 3
+  end
+  it "gives the first name of crypto: BTC" do
+    expect(scrap_names(page)[0]).to eq("BTC")
+  end
+end
+
+describe "the scrap_values method" do
+  it "gives at least 3 values" do
+    expect(scrap_values(page).length).to be > 3
+  end
+  it "gives the value of the 3rd crypto (USDT): $1.00" do
+    expect(scrap_values(page)[2]).to eq("$1.00")
+  end
+end
+
+describe "the link_cryto method" do
+  it "should be an hash" do
+    expect((link_crypto(scrap_names(page), scrap_values(page))).class).to eq(Hash)
+  end
+  it "gives at least 10 values" do
+    expect((link_crypto(scrap_names(page), scrap_values(page))).length).to be > 10
+  end
+end
+
+describe "the perform method" do
+  it "should not raise any error" do
+    expect{perform(page)}.to_not raise_error(StandardError)
+  end
+  it "should not give empty values (nil)" do
+    expect(perform(page)).to_not eq(nil)
   end
 end
