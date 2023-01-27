e93a9d49c5c7ad72e9a8996d929763d9f4527d02
Suziwan <christe.suzanne@gmail.com>
2023-01-27 00:30:47 +0000

    Townhall exercise

--- lib/townhall_emails.rb
+++ lib/townhall_emails.rb
@@ -1,16 +1,38 @@
 require 'nokogiri'
 require 'open-uri'
 
-page = Nokogiri::HTML(URI.open('https://www.annuaire-des-mairies.com/val-d-oise.html'))
+department_url = Nokogiri::HTML(URI.open('https://www.annuaire-des-mairies.com/val-d-oise.html'))
 townhall_url = Nokogiri::HTML(URI.open('https://www.annuaire-des-mairies.com/95/avernes.html'))
 
 def get_townhall_email(townhall_url)
-  email = townhall_url.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').text
-  puts email
+  townhall_url.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').text
 end
 
-def get_townhall_urls(page)
-  urls = 
+def get_townhall_urls(department_url)
+  urls_array = []
+  department_url.xpath('//tr[3]/td//td[2]//td//a/@href').each do |link|
+    urls_array << link.content
+  end
+  urls_array.map! { |string| string.delete_prefix('.') } # Remove the initial "."
+  urls_array.map! { |hash| hash.to_s.prepend("http://annuaire-des-mairies.com") } # Add the general url
+  return urls_array
 end
+
+=begin
+def get_all_emails(url_to_use)
+  emails_array = []
+  url_to_use.each do |link|
+    emails_array << link.content
+  end
+  puts emails_array
+end
+
+def perform(department_url)
+  get_all_emails(get_townhall_urls(department_url))
+end
+=end 
 
 get_townhall_email(townhall_url)
+get_townhall_urls(department_url)
+#get_all_emails(url_to_use)
+#perform(department_url)
--- spec/townhall_emails_spec.rb
+++ spec/townhall_emails_spec.rb
@@ -1,7 +1,28 @@
 require_relative '../lib/townhall_emails'
 
-describe "the method to test" do
-  it "does the test" do
-    #expect(hello).to eq("Hello world!")
+department_url = Nokogiri::HTML(URI.open('https://www.annuaire-des-mairies.com/val-d-oise.html'))
+townhall_url = Nokogiri::HTML(URI.open('https://www.annuaire-des-mairies.com/95/avernes.html'))
+
+describe "the get_townhall_email method" do
+  it "should give me the email address of Avernes" do
+    expect(get_townhall_email(townhall_url)).to eq("mairie.avernes@orange.fr")
   end
 end
+
+describe "the get_townhall_url method" do
+  it "should give me the URL of the first townhall" do
+    expect(get_townhall_urls(department_url)[0]).to eq("http://annuaire-des-mairies.com/95/ableiges.html")
+  end
+end
+
+#describe "the get_all_emails method" do
+#  it "should get an email address" do
+#    expect(get_all_emails(url_to_use)).to eq("Something")
+#  end
+#end
+
+#describe "the perform method" do
+#  it "should test something" do
+#    expect(perform(department_url)).to eq("Something")
+#  end
+#end
